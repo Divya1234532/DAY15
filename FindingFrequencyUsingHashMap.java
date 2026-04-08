a1f4fa3c5e2ded198c36f0faffd0c13103e9d621
Divya1234532 <dd1320983@gmail.com>
2026-04-08 11:06:58 +0530

    Update FindingFrequencyUsingHashMap.java

--- FindingFrequencyUsingHashMap.java
+++ FindingFrequencyUsingHashMap.java
@@ -6,14 +6,15 @@ public class FindingFrequencyUsingMapInterface {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++)
         {
-            if(map.containsKey(arr[i]))
+            /*if(map.containsKey(arr[i]))
             {
                 map.put(arr[i],map.get(arr[i])+1);
             }
             else
             {
                 map.put(arr[i],1);
-            }
+            }*/
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);//optimal method
         }
         for(Map.Entry<Integer,Integer> entry:map.entrySet())
         {
